8dd85596253dc67a6f394cbd4eb6a9df9b19ad5c
e10withadot <e10withadot@gmail.com>
2025-03-18 13:48:01 +0200

    Added template for filepath entry

--- installer/mainGUI.py
+++ installer/mainGUI.py
@@ -3,8 +3,28 @@ import baseGUI as gui
 
 class Page1(gui.Page):
     def __init__(self, root):
+        desc= '''Welcome to the builder wizard for the Super Mario Bros. C recompilation, "Super Mario Bros. C Plus".
+Please enter the file path for your Super Mario Bros. ROM below.'''
         elements = [
-            gui.Element(tk.Label(root, text='Super Mario Bros C Plus Builder'), {"row": 0, "column": 0})
+            gui.Element(tk.Label(root,
+                            text='Super Mario Bros C Plus Builder',
+                            pady=15,
+                            font=("TkDefaultFont", 15)),
+                            {"row": 0, "column": 0, "columnspan": 2}
+                        ),
+            gui.Element(tk.Label(root,
+                            text= desc),
+                            {"row": 1, "column": 0, "columnspan": 2}),
+            gui.Element(tk.Entry(root,
+                                justify='right'),
+                            {"row": 2, "column": 0, "sticky": "e"}
+                        ),
+            gui.Element(tk.Button(root,
+                                text='Browse...',
+                                anchor='w',
+                                justify='left'),
+                            {"row": 2, "column": 1, "sticky": "w"}
+                        )
         ] 
         super().__init__(elements)
 
